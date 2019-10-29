485bd67e60fc562a74a513833ee48125db35e7ee
James Paik <jpaik456@gmail.com>
2019-10-29 12:15:28 -0400

    Fix Reddit help commands

--- commands/help.js
+++ commands/help.js
@@ -12,9 +12,9 @@ var HELP_COMMANDS = {
                     help_text = "You can use this command like: `!stock [TICKER] (optional)[info|NUM_MONTHS]`";
                     break;
                 case 'reddit':
-                    help_text = `You can use this command like: \`!reddit [SUBREDDIT] [stop|now|time in HH: MM format 24 hour standard(multiple times separated by commas)] (optional:default=hot)[hot|top|rising]\`.
-                    \nE.G: \`!reddit funny 17:00 top\` will post the top reddit post from /r/funny at 5:00PM EST daily.
-                    \nE.G: \`!reddit funny now top\` will post the top reddit post instantly.`;
+                    help_text = `You can use this command like: \`!reddit [SUBREDDIT] [stop|now|time in HH:MM format 24 hour standard(multiple times separated by commas)] (optional:default=hot)[hot|top|rising]\`.`;
+                    help_text += `\nE.G: \`!reddit funny 17:00 top\` will post the top reddit post from /r/funny at 5:00PM EST daily.`;
+                    help_text += `\nE.G: \`!reddit funny now top\` will post the top reddit post instantly.`;
                     break;
                 case 'weather':
                     help_text = "You can use this command like: `!weather (optional)[tomorrow|week|schedule|set|default] (optional:default=I)[UNIT(M=Metric|S=Scientific|I=Imperial)] [CITY_NAME]`";
